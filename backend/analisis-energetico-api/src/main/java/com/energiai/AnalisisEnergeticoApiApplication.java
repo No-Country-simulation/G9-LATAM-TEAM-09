84c43979578c182cda073271583505f9cd99b676
Leandro-tech687 <pfccomputing@gmail.com>
2026-07-24 00:28:07 -0300

    Inicialización del Proyecto V1

---
+++ backend/analisis-energetico-api/src/main/java/com/energiai/AnalisisEnergeticoApiApplication.java
@@ -0,0 +1,13 @@
+package com.energiai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalisisEnergeticoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalisisEnergeticoApiApplication.class, args);
+	}
+
+}
